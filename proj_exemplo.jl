c03ebedf4db7a4fd94d9cd9d8c1be41ae3b57ce8
fillipepierin <bianekpierin@gmail.com>
2019-05-28 12:25:13 -0300

    Update proj_exemplo.jl

--- proj_exemplo.jl
+++ proj_exemplo.jl
@@ -1,7 +1,7 @@
 # Projeto Numérica I - LU com pivoteamento parcial e completo para aumentar a esparsidade
 #                      Exemplo usado no texto do projeto.
 
-using Plots
+using Plots, Printf
 pyplot(size=(500,400)) # size=(400,300)
 using LinearAlgebra
 using SparseArrays
@@ -24,6 +24,20 @@ function main()
     (L, U) = sep_LU(A);
     println("Esparsidade da Matriz L: ", esp(L))
     println("Esparsidade da Matriz U: ", esp(U))
+    
+    println("Comparação de valores do parâmetro Threshold.")
+    s = @sprintf("%-1s %1s  %1s\n", "Tau", "esp(L)", "esp(U)")
+    print(s)
+    for l in 0.1:0.1:1
+        A = [2.0 0.0 2.0 0.0 0.0;
+            3.0 1.0 4.0 0.0 0.0;
+            0.0 0.0 -3.0 2.0 0.0;
+            0.0 0.0 0.0 1.0 0.0;
+            0.0 4.0 2.0 2.0 1.0];
+        (A, p, q) = LU_pivo_mark(A);
+        (L, U) = sep_LU(A);
+        println(l, "  ", esp(L), "  ", esp(U))
+    end
 end
 
 main()
